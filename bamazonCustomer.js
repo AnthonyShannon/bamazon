dfaafcceeeed90dd5a981f5b507c9985b84c08ee
John Calvin <ashannon1990@gmail.com>
2019-05-15 15:57:24 -0400

    Added inquirer and it is asking for the item I want

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,13 +3,32 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: '********',
+    password: 'alphaCoder90',
     database: 'bamazon'
 });
 
-connection.connect()
-connection.query('SELECT * from products', function (error, results, fields) {
-    if (error) throw error;
-    console.table(results);
-});
+connection.connect(function(err) {
+    if (err) throw err;
+    
+  });
+  
+  function start(){
+    connection.query('SELECT * from products', function (error, results, fields) {
+        if (error) throw error;
+        console.log("Current inventory:")
+        console.table(results);
+    
+
+    inquirer
+        .prompt({
+            name: "itemID",
+            type: "input",
+            message: "What is the ID of the item you would like?"
+        })
+  })};
+  start();
+// connection.query('SELECT * from products', function (error, results, fields) {
+//     if (error) throw error;
+//     console.table(results);
+// });
 connection.end()

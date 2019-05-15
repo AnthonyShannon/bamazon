6c24330ea9ba0a272d7adfb241d86511e8c8e8fb
John Calvin <ashannon1990@gmail.com>
2019-05-15 16:11:28 -0400

    Small change

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: 'alphaCoder90',
+    password: '*****',
     database: 'bamazon'
 });
 
